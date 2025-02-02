3949e48b7268786f0c9460c82b9ff7c8503f8a3a
SujanParajuli0 <sujan.parajuli2070@gmail.com>
2025-02-02 13:33:20 -0500

    Update app.py

--- streamlit/route_finder/app.py
+++ streamlit/route_finder/app.py
@@ -19,7 +19,7 @@ button = st.button('Get Directions')
 placeholder = st.empty()
 
 
-ORS_API_KEY = st.secrets['ORS_API_KEY']
+ORS_API_KEY = st.secrets['5b3ce3597851110001cf6248aed5d7f73ee3492aaa88b369dea7f912']
 
 @st.cache_data
 def geocode(query):
